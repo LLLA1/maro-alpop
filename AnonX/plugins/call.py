a0814f959c8b7d86395c72db84520b3f823ce252
newproject23 <143152902+newproject23@users.noreply.github.com>
2024-01-06 22:08:06 +0200

    Update call.py

--- AnonX/plugins/call.py
+++ AnonX/plugins/call.py
@@ -9,17 +9,17 @@ from AnonX.utils.database import get_assistant
 
 @app.on_message(filters.voice_chat_started)
 async def stcall(client: Client, message: Message): 
-      Startt = "فتحو المكالمه وهيصدعونا"
+      Startt = "فتحو المكالمه وهيرغو ولاد العرص"
       await message.reply_text(Startt)
 
 @app.on_message(filters.voice_chat_ended)
 async def encall(client: Client, message: Message): 
-      Enddd = "قفلتو المكالمه لي ما لسي بدري"
+      Enddd = "قفلتو المكالمه لي ما لسه بدري يولاد الاحبه"
       await message.reply_text(Enddd)
 
 @app.on_message(filters.voice_chat_members_invited)
 async def zoharyy(client: Client, message: Message): 
-           text = f"- قام {message.from_user.mention}\n - بدعوة : "
+           text = f"- قام {message.from_user.mention}\n -  بدعوة الخول : "
            x = 0
            for user in message.voice_chat_members_invited.users:
              try:
