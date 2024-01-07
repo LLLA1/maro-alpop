bb2b0979c41743934a2190258dd40643b75eff54
newproject23 <143152902+newproject23@users.noreply.github.com>
2024-01-07 03:18:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "cr")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "alpop")
 
 OWNER_ID = list(
-  map(int, getenv("OWNER_ID", "").split())) + [5881570606]
+  map(int, getenv("OWNER_ID", "").split())) + [5964879906]
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/newproject23/Tom")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PP_G3")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/JK_ZG")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/source_alpop")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/bar_alpop")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/c973727bddf803e028539.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/f698f60484b7aef0d6f29.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://telegra.ph/file/56d1760224589ee370186.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/f698f60484b7aef0d6f29.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://telegra.ph/file/e7bb54b34faadd2c9b199.jpg"
+            START_IMG_URL = "https://telegra.ph/file/f698f60484b7aef0d6f29.jpg"
